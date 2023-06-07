03f23ac4b7c9d5ae6ebc4572c37a30a181c76da8
saba3324 <133587965+saba3324@users.noreply.github.com>
2023-06-07 10:50:38 -0700

    Create App.vue

---
+++ vuejs-tutorial/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <Header title="Vue App" action="Get started" color="lightblue" bgColor="darkblue" />
+    </div>
+    <div class="container">
+        <Header title="Todo list" action="Add Task" />
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+    name: 'App',
+    components: {
+        Header
+    }
+}
+</script>
+
+<style>
+#app {
+    text-align: center;
+    margin-top: 60px;
+}
+
+.container {
+    border: 1px solid steelblue;
+    padding: 30px;
+    max-width: 500px;
+    margin: 20px auto;
+}
+</style>
