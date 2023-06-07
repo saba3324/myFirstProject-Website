8e45c58d2ba8bdf406757973357d9ed4df7ca8ca
saba3324 <133587965+saba3324@users.noreply.github.com>
2023-06-07 10:51:26 -0700

    Create Header.vue

---
+++ vuejs-tutorial/src/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h1 :style="{ color: color }" >{{ title }}</h1>
+        <button @click="handleClick()" :style="{ background: bgColor }">{{ action }}</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        action: String,
+        color: String,
+        bgColor: String
+    },
+    methods: {
+        handleClick() {
+            console.log("button clicked")
+        }
+    }
+}
+</script>
+
+<style>
+button {
+    padding: 10px 20px;
+    font-size: 15px;
+    background: seagreen;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+h1 {
+    color: yellowgreen;
+}
+</style>
